df024d75d705e9004cec162c2f33d9a05e882b97
giovannioddo90 <giovannioddo90@gmail.com>
2025-05-27 23:26:22 -0400

    add framerate limiter

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
-        screen.fill((0, 0, 0))
+        screen.fill("black")
         pygame.display.flip()
-
+        
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
 
